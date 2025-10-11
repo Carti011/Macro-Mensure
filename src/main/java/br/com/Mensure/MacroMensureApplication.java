41dbd3ccbd7f84694184b18c20dde2303d80787e
Weslley Cardoso <weslleycardoso011@gmail.com>
2025-10-10 23:58:44 -0300

    Initial commit: Estrutura do projeto e entidade MedicaoAmostra

---
+++ src/main/java/br/com/Mensure/MacroMensureApplication.java
@@ -0,0 +1,13 @@
+package br.com.Mensure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroMensureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroMensureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Mensure/MacroMensureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Mensure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroMensureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
