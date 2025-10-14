dad2d6f421d44f158335434a50d09d4141c60980
Weslley Cardoso <weslleycardoso011@gmail.com>
2025-10-14 10:04:23 -0300

    feat(domain): Adiciona entidade Paciente, Enum e Repository

---
+++ src/main/java/br/com/mensure/domain/entity/Paciente.java
@@ -0,0 +1,42 @@
+package br.com.mensure.domain.entity;
+
+import br.com.mensure.domain.enums.Genero;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "PACIENTES")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Genero genero;
+
+    // Relacionamento 1:N
+    @OneToMany(mappedBy = "paciente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MedicaoAmostra> medicoes = new ArrayList<>();
+}
---
+++ src/main/java/br/com/mensure/domain/enums/Genero.java
@@ -0,0 +1,7 @@
+package br.com.mensure.domain.enums;
+
+public enum Genero {
+    MASCULINO,
+    FEMININO,
+    OUTRO
+}
