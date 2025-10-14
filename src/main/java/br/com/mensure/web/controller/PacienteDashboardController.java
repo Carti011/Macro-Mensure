49ebee164300ab7e1f73e232697ba3e3546590da
Weslley Cardoso <weslleycardoso011@gmail.com>
2025-10-14 10:06:37 -0300

    feat(web): Adiciona interface com Thymeleaf para CRUD de Paciente

---
+++ src/main/java/br/com/mensure/web/controller/PacienteDashboardController.java
@@ -0,0 +1,62 @@
+package br.com.mensure.web.controller;
+
+import br.com.mensure.api.dto.PacienteRequestDTO;
+import br.com.mensure.api.dto.PacienteResponseDTO;
+import br.com.mensure.domain.enums.Genero;
+import br.com.mensure.domain.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/pacientes")
+public class PacienteDashboardController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public String showPacienteList(Model model) {
+        model.addAttribute("listaDePacientes", pacienteService.findAll());
+        return "pacientes";
+    }
+
+    @GetMapping("/novo")
+    public String showPacienteCreateForm(Model model) {
+        model.addAttribute("paciente", new PacienteResponseDTO());
+        model.addAttribute("generoOptions", Genero.values());
+        return "paciente-form";
+    }
+
+    @PostMapping("/salvar")
+    public String savePaciente(@ModelAttribute("paciente") PacienteRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+        pacienteService.create(requestDTO);
+        redirectAttributes.addFlashAttribute("successMessage", "Paciente cadastrado com sucesso!");
+        return "redirect:/pacientes";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String showPacienteEditForm(@PathVariable Long id, Model model) {
+        PacienteResponseDTO pacienteDTO = pacienteService.findById(id);
+        model.addAttribute("paciente", pacienteDTO);
+        model.addAttribute("id", id);
+        model.addAttribute("generoOptions", Genero.values());
+        return "paciente-form";
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public String updatePaciente(@PathVariable Long id, @ModelAttribute("paciente") PacienteRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+        pacienteService.update(id, requestDTO);
+        redirectAttributes.addFlashAttribute("successMessage", "Paciente atualizado com sucesso!");
+        return "redirect:/pacientes";
+    }
+
+    @PostMapping("/excluir/{id}")
+    public String deletePaciente(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        pacienteService.delete(id);
+        redirectAttributes.addFlashAttribute("successMessage", "Paciente excluído com sucesso!");
+        return "redirect:/pacientes";
+    }
+}
