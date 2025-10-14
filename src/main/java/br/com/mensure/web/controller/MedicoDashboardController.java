6ebb13793f0f62778e0ed18ef6109bffa4946cd9
Weslley Cardoso <weslleycardoso011@gmail.com>
2025-10-14 10:09:14 -0300

    feat(web): Adiciona interface com Thymeleaf para CRUD de Médico

---
+++ src/main/java/br/com/mensure/web/controller/MedicoDashboardController.java
@@ -0,0 +1,59 @@
+package br.com.mensure.web.controller;
+
+import br.com.mensure.api.dto.MedicoRequestDTO;
+import br.com.mensure.api.dto.MedicoResponseDTO;
+import br.com.mensure.domain.service.MedicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/medicos")
+public class MedicoDashboardController {
+
+    @Autowired
+    private MedicoService medicoService;
+
+    @GetMapping
+    public String showMedicoList(Model model) {
+        model.addAttribute("listaDeMedicos", medicoService.findAll());
+        return "medicos";
+    }
+
+    @GetMapping("/novo")
+    public String showMedicoCreateForm(Model model) {
+        model.addAttribute("medico", new MedicoResponseDTO());
+        return "medico-form";
+    }
+
+    @PostMapping("/salvar")
+    public String saveMedico(@ModelAttribute("medico") MedicoRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+        medicoService.create(requestDTO);
+        redirectAttributes.addFlashAttribute("successMessage", "Médico cadastrado com sucesso!");
+        return "redirect:/medicos";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String showMedicoEditForm(@PathVariable Long id, Model model) {
+        MedicoResponseDTO medicoDTO = medicoService.findById(id);
+        model.addAttribute("medico", medicoDTO);
+        model.addAttribute("id", id);
+        return "medico-form";
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public String updateMedico(@PathVariable Long id, @ModelAttribute("medico") MedicoRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+        medicoService.update(id, requestDTO);
+        redirectAttributes.addFlashAttribute("successMessage", "Médico atualizado com sucesso!");
+        return "redirect:/medicos";
+    }
+
+    @PostMapping("/excluir/{id}")
+    public String deleteMedico(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        medicoService.delete(id);
+        redirectAttributes.addFlashAttribute("successMessage", "Médico excluído com sucesso!");
+        return "redirect:/medicos";
+    }
+}
